3eb1b9e38688c0a48b473160364d5700349cc9dc
Willination <traviswilliams130691@gmail.com>
2023-11-20 00:31:33 +0200

    changes

--- src/app/imperial-to-metric-form/imperial-to-metric-form.component.ts
+++ src/app/imperial-to-metric-form/imperial-to-metric-form.component.ts
@@ -31,10 +31,9 @@ export class ImperialToMetricFormComponent implements OnInit {
     this.conversionService.getConversionModel().subscribe((model) => {
       this.conversionModel = model;
       this.updateUnits();
-      this.cdr.detectChanges(); // Trigger change detection
+      this.cdr.detectChanges();
     });
   }
-
 
   updateUnits() {
     switch (this.conversionModel.conversionType) {
@@ -58,105 +57,13 @@ export class ImperialToMetricFormComponent implements OnInit {
   }
 
   convert() {
-
     if (!this.conversionModel.conversionType || !this.conversionModel.fromUnit || !this.conversionModel.toUnit || !this.conversionModel.fromValue) {
       this.showError = true;
       return;
     }
-
     this.showError = false;
-
-    /*const fromValue = this.conversionModel.fromValue;*/
-
     this.convertImperialToMetric();
-
-  /*  switch (this.conversionModel.conversionType) {
-      case 'Temperature':
-        this.convertTemperature(fromValue);
-        break;
-      case 'Length':
-        this.convertLength(fromValue);
-        break;
-      case 'Weight':
-        this.convertWeight(fromValue);
-        break;
-      default:
-        // Handle other conversion types if needed
-        break;
-    }*/
   }
-
-/*  convertTemperature(value: number) {
-    if (this.conversionModel.fromUnit === 'Fahrenheit' && this.conversionModel.toUnit === 'Celsius') {
-      this.conversionModel.toValue = (value - 32) * 5 / 9;
-    } else if (this.conversionModel.fromUnit === 'Fahrenheit' && this.conversionModel.toUnit === 'Kelvin') {
-      this.conversionModel.toValue = (value - 32) * 5 / 9 + 273.15;
-    } else if (this.conversionModel.fromUnit === 'Kelvin' && this.conversionModel.toUnit === 'Celsius') {
-      this.conversionModel.toValue = value - 273.15;
-    } else if (this.conversionModel.fromUnit === 'Kelvin' && this.conversionModel.toUnit === 'Fahrenheit') {
-      this.conversionModel.toValue = (value - 273.15) * 9 / 5 + 32;
-    } else if (this.conversionModel.fromUnit === 'Celsius' && this.conversionModel.toUnit === 'Fahrenheit') {
-      this.conversionModel.toValue = (value * 9 / 5) + 32;
-    } else if (this.conversionModel.fromUnit === 'Celsius' && this.conversionModel.toUnit === 'Kelvin') {
-      this.conversionModel.toValue = value + 273.15;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
-
-/*  convertLength(value: number) {
-    const centimeterToInch = 0.393701;
-    const mileToKilometer = 1.60934;
-    const kilometerToMile = 0.621371;
-    const inchToCentimeter = 2.54;
-    if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Inches') {
-      this.conversionModel.toValue = value / centimeterToInch;
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Feet') {
-      this.conversionModel.toValue = value / (centimeterToInch * 12);
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Yards') {
-      this.conversionModel.toValue = value / (centimeterToInch * 36);
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Miles') {
-      this.conversionModel.toValue = value / (centimeterToInch * 100000) * kilometerToMile;
-    } else if (this.conversionModel.fromUnit === 'Miles' && this.conversionModel.toUnit === 'Inches') {
-      this.conversionModel.toValue = value * inchToCentimeter / 100000 / centimeterToInch;
-    } else if (this.conversionModel.fromUnit === 'Miles' && this.conversionModel.toUnit === 'Centimeters') {
-      this.conversionModel.toValue = value * inchToCentimeter / 100000;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
-
-/*  convertWeight(value: number) {
-    const kilogramToPound = 2.20462;
-    const gramToMilligram = 0.001;
-    const tonToKilogram = 907.185;
-    const poundToKilogram = 0.453592;
-
-    if (this.conversionModel.fromUnit === 'Kilograms' && this.conversionModel.toUnit === 'Pounds') {
-      this.conversionModel.toValue = value * kilogramToPound;
-    } else if (this.conversionModel.fromUnit === 'Kilograms' && this.conversionModel.toUnit === 'Milligrams') {
-      this.conversionModel.toValue = value * 1000 * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'Kilograms' && this.conversionModel.toUnit === 'Metric Tons') {
-      this.conversionModel.toValue = value / tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Grams') {
-      this.conversionModel.toValue = value * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Kilograms') {
-      this.conversionModel.toValue = value * gramToMilligram / 1000;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Metric Tons') {
-      this.conversionModel.toValue = value * gramToMilligram / 1000 / tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Kilograms') {
-      this.conversionModel.toValue = value * tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Milligrams') {
-      this.conversionModel.toValue = value * tonToKilogram * 1000 * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Pounds') {
-      this.conversionModel.toValue = value * tonToKilogram * poundToKilogram;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
 
   resetForm() {
     this.conversionService.resetForm();
@@ -168,5 +75,4 @@ export class ImperialToMetricFormComponent implements OnInit {
       this.conversionModel.toValue = response.toValue;
     });
   }
-
 }
--- src/app/metric-to-imperial-form/metric-to-imperial-form.component.ts
+++ src/app/metric-to-imperial-form/metric-to-imperial-form.component.ts
@@ -31,7 +31,7 @@ export class MetricToImperialFormComponent implements OnInit {
     this.conversionService.getConversionModel().subscribe((model) => {
       this.conversionModel = model;
       this.updateUnits();
-      this.cdr.detectChanges(); // Trigger change detection
+      this.cdr.detectChanges();
     });
   }
 
@@ -57,106 +57,13 @@ export class MetricToImperialFormComponent implements OnInit {
   }
 
   convert() {
-
     if (!this.conversionModel.conversionType || !this.conversionModel.fromUnit || !this.conversionModel.toUnit || !this.conversionModel.fromValue) {
       this.showError = true;
       return;
     }
-
     this.showError = false;
-
-  /*  const fromValue = this.conversionModel.fromValue;*/
-
     this.convertMetricToImperial();
-
-/*    switch (this.conversionModel.conversionType) {
-      case 'Temperature':
-        this.convertTemperature(fromValue);
-        break;
-      case 'Length':
-        this.convertLength(fromValue);
-        break;
-      case 'Weight':
-        this.convertWeight(fromValue);
-        break;
-      default:
-        break;
-    }*/
   }
-
-/*  convertTemperature(value: number) {
-    if (this.conversionModel.fromUnit === 'Celsius' && this.conversionModel.toUnit === 'Fahrenheit') {
-      this.conversionModel.toValue = (value * 9 / 5) + 32;
-    } else if (this.conversionModel.fromUnit === 'Celsius' && this.conversionModel.toUnit === 'Kelvin') {
-      this.conversionModel.toValue = value + 273.15;
-    } else if (this.conversionModel.fromUnit === 'Kelvin' && this.conversionModel.toUnit === 'Celsius') {
-      this.conversionModel.toValue = value - 273.15;
-    } else if (this.conversionModel.fromUnit === 'Kelvin' && this.conversionModel.toUnit === 'Fahrenheit') {
-      this.conversionModel.toValue = (value - 273.15) * 9 / 5 + 32;
-    } else if (this.conversionModel.fromUnit === 'Fahrenheit' && this.conversionModel.toUnit === 'Celsius') {
-      this.conversionModel.toValue = (value - 32) * 5 / 9;
-    } else if (this.conversionModel.fromUnit === 'Fahrenheit' && this.conversionModel.toUnit === 'Kelvin') {
-      this.conversionModel.toValue = (value - 32) * 5 / 9 + 273.15;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
-
-/*  convertLength(value: number) {
-    const inchToCentimeter = 2.54;
-    const kilometerToMile = 0.621371;
-
-    if (this.conversionModel.fromUnit === 'Inches' && this.conversionModel.toUnit === 'Centimeters') {
-      this.conversionModel.toValue = value * inchToCentimeter;
-    } else if (this.conversionModel.fromUnit === 'Inches' && this.conversionModel.toUnit === 'Feet') {
-      this.conversionModel.toValue = value / 12;
-    } else if (this.conversionModel.fromUnit === 'Inches' && this.conversionModel.toUnit === 'Yards') {
-      this.conversionModel.toValue = value / 36;
-    } else if (this.conversionModel.fromUnit === 'Inches' && this.conversionModel.toUnit === 'Miles') {
-      this.conversionModel.toValue = value * inchToCentimeter / 100000 * kilometerToMile;
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Inches') {
-      this.conversionModel.toValue = value / inchToCentimeter;
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Feet') {
-      this.conversionModel.toValue = value / (inchToCentimeter * 12);
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Yards') {
-      this.conversionModel.toValue = value / (inchToCentimeter * 36);
-    } else if (this.conversionModel.fromUnit === 'Centimeters' && this.conversionModel.toUnit === 'Miles') {
-      this.conversionModel.toValue = value / (inchToCentimeter * 100000) * kilometerToMile;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
-
-  /*convertWeight(value: number) {
-    const poundToKilogram = 0.453592;
-    const gramToMilligram = 1000;
-    const tonToKilogram = 907.185;
-
-    if (this.conversionModel.fromUnit === 'Pounds' && this.conversionModel.toUnit === 'Kilograms') {
-      this.conversionModel.toValue = value * poundToKilogram;
-    } else if (this.conversionModel.fromUnit === 'Pounds' && this.conversionModel.toUnit === 'Milligrams') {
-      this.conversionModel.toValue = value * poundToKilogram * 1000 * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'Pounds' && this.conversionModel.toUnit === 'Metric Tons') {
-      this.conversionModel.toValue = value * poundToKilogram / tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Grams') {
-      this.conversionModel.toValue = value * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Kilograms') {
-      this.conversionModel.toValue = value * gramToMilligram / 1000 * poundToKilogram;
-    } else if (this.conversionModel.fromUnit === 'Ounces' && this.conversionModel.toUnit === 'Metric Tons') {
-      this.conversionModel.toValue = value * gramToMilligram / 1000 * poundToKilogram / tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Kilograms') {
-      this.conversionModel.toValue = value * tonToKilogram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Milligrams') {
-      this.conversionModel.toValue = value * tonToKilogram * 1000 * gramToMilligram;
-    } else if (this.conversionModel.fromUnit === 'US Short Tons' && this.conversionModel.toUnit === 'Pounds') {
-      this.conversionModel.toValue = value * tonToKilogram / poundToKilogram;
-    }
-    if (this.conversionModel.toValue != null) {
-      this.conversionModel.toValue = Number((this.conversionModel.toValue.toFixed(2)).toLocaleString());
-    }
-  }*/
 
   resetForm() {
     this.conversionService.resetForm();
